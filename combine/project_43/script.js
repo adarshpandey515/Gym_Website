8b72bdda04656957e19b23faaa390e5ccd1f8eb7
Adarsh Pandey <pandeyadarsh515@gmail.com>
2023-06-08 16:01:06 +0530

    hello

---
+++ combine/project_43/script.js
@@ -0,0 +1,16 @@
+// 43
+// // console.log("hello world ")
+// // console.log(document.body.firstElementChild)
+// let a =document.getElementsByTagName("div")[0]  
+// // a.innerHTML=a.innerHTML+"<h1> hello world </h1>";
+// let div=document.createElement('div');
+// div.innerHTML='<h2>Adarsh Pandey </h2>';
+// a.replaceWith(div);
+// 44
+
+// first.insertAdjacentHTML('beforeend','<div class="text"> before end </div>')
+// first.insertAdjacentHTML('afterend','<div class="text"> after end </div>')
+// document.body.firstElementChild.remove()
+
+
+//45
